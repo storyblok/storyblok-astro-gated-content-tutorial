34220cb3a9fc7f2b501e92446cc3e556e1bcc5f3
Dipankar Maikap <45673791+dipankarmaikap@users.noreply.github.com>
2024-07-25 14:50:16 +0530

    Local ssl Option added for Storyblok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,18 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import mkcert from "vite-plugin-mkcert";
+import { loadEnv } from "vite";
+
+const { RUNNING_LOCALLY, STORYBLOK_ACESS_TOKEN, STORYBLOK_IS_PREVIEW } =
+  loadEnv(process.env.NODE_ENV, process.cwd(), "");
+const isLocal = RUNNING_LOCALLY === "yes";
+const isPreview = STORYBLOK_IS_PREVIEW === "yes";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  vite: {
+    plugins: [...(isLocal && [mkcert()])],
+  },
 });
