d6f7dbe4da8adab290bab08bdcae844672e23d55
Dipankar Maikap <45673791+dipankarmaikap@users.noreply.github.com>
2024-07-25 15:07:04 +0530

    Storyblok adeed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,32 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
 import mkcert from "vite-plugin-mkcert";
 import { loadEnv } from "vite";
+import vercel from "@astrojs/vercel/serverless";
+import storyblok from "@storyblok/astro";
 
-const { RUNNING_LOCALLY, STORYBLOK_ACESS_TOKEN, STORYBLOK_IS_PREVIEW } =
-  loadEnv(process.env.NODE_ENV, process.cwd(), "");
+const { RUNNING_LOCALLY, STORYBLOK_ACESS_TOKEN } = loadEnv(
+  process.env.NODE_ENV,
+  process.cwd(),
+  ""
+);
+//This is just for local SSL, dont need this in production
 const isLocal = RUNNING_LOCALLY === "yes";
-const isPreview = STORYBLOK_IS_PREVIEW === "yes";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    storyblok({
+      accessToken: STORYBLOK_ACESS_TOKEN,
+      components: {
+        article: "storyblok/Article",
+      },
+    }),
+  ],
   vite: {
     plugins: [...(isLocal && [mkcert()])],
   },
+  output: "server",
+  adapter: vercel(),
 });
