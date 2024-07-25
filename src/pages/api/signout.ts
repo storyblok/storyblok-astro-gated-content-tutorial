c20d8d7db2ac93d46cf7d1a65424ef8475b99f28
Dipankar Maikap <45673791+dipankarmaikap@users.noreply.github.com>
2024-07-25 20:23:52 +0530

    enhance-ui

---
+++ src/pages/api/signout.ts
@@ -0,0 +1,25 @@
+import type { APIContext } from "astro";
+import { lucia } from "~/lib/auth";
+
+export async function POST(context: APIContext): Promise<Response> {
+  if (!context.locals.session) {
+    return new Response(null, {
+      status: 401,
+    });
+  }
+
+  await lucia.invalidateSession(context.locals.session.id);
+
+  const sessionCookie = lucia.createBlankSessionCookie();
+  context.cookies.set(
+    sessionCookie.name,
+    sessionCookie.value,
+    sessionCookie.attributes
+  );
+
+  return new Response(
+    JSON.stringify({
+      sucess: true,
+    })
+  );
+}
